14ec706621034d7651f78a7c6e2056c6e8fedeff
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2025-07-21 22:16:07 -0700

    Add BookDb class for book CRUD operations
    Introduces a static class `BookDb` in the `BookstoreApp.Database` namespace.
    Adds an asynchronous method `GetBooksAsync` to retrieve and sort books from the database using Entity Framework Core.

---
+++ BookstoreApp/Database/BookDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreApp.Database
+{
+    /// <summary>
+    /// Provides methods for Book CRUD functionality
+    /// </summary>
+    public static class BookDb
+    {
+        /// <summary>
+        /// Retrieves a list of books from the database
+        /// sorted in alphabetical order by Title (A - Z)
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<Book>> GetBooksAsync()
+        {
+            using BookStoreDb db = new();
+
+            // Get all books from the database
+            List<Book> books = await db.Books.OrderBy(b => b.Title).ToListAsync(); // EF Core method syntax
+
+            return books;
+        }
+    }
+}
